88605d6347c92257c6fe8d04c61ad3de6af11287
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-02-21 19:28:59 -0300

    feat: :sparkles: Configure google fonts

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function Home() {
   return (
-    <View className="flex-1 bg-red-700">
-      <Text>Home</Text>
+    <View className="flex-1 bg-gray-900 items-center justify-center">
+      <Text className="text-white">Show</Text>
     </View>
   )
 }
