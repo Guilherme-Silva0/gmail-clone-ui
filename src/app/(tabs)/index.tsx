909fec46e8b4e03c48f03eac63671ad04b448a05
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-02-22 19:07:45 -0300

    feat: :sparkles: Add MenuButton component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { Input } from '@/components/Input'
+import { MenuButton } from '@/components/MenuButton'
 import { View } from 'react-native'
 
 export default function Home() {
   return (
     <View className="flex-1 bg-gray-900 pt-14 p-4">
       <Input>
+        <MenuButton />
         <Input.Field placeholder="Pesquisar no e-mail..." />
       </Input>
     </View>
