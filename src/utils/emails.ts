bb75465ab6b9f014dea5e10955282f9bf51b1aab
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-02-23 16:14:22 -0300

    feat: :monocle_face: Create Email data

---
+++ src/utils/emails.ts
@@ -0,0 +1,180 @@
+export const EMAILS = [
+  {
+    id: '1',
+    name: 'Jhon Smith',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/65.jpg',
+    marker: true,
+    subject: 'What is Lorem Ipsum',
+    message:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    star: true,
+    date: '23 de Fev.',
+  },
+  {
+    id: '2',
+    name: 'Anne Clarke',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/66.jpg',
+    marker: false,
+    subject: 'Where does it come from',
+    message:
+      'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+    star: true,
+    date: '24 de Fev.',
+  },
+  {
+    id: '3',
+    name: 'Mike Johnson',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/45.jpg',
+    marker: false,
+    subject: 'Why do we use it?',
+    message:
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    star: false,
+    date: '24 de Fev.',
+  },
+  {
+    id: '4',
+    name: 'Jhon Smith',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/65.jpg',
+    marker: false,
+    subject: 'What is Lorem Ipsum',
+    message:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    star: true,
+    date: '23 de Fev.',
+  },
+  {
+    id: '5',
+    name: 'Anne Clarke',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/66.jpg',
+    marker: false,
+    subject: 'Where does it come from',
+    message:
+      'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+    star: true,
+    date: '24 de Fev.',
+  },
+  {
+    id: '6',
+    name: 'Mike Johnson',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/45.jpg',
+    marker: false,
+    subject: 'Why do we use it?',
+    message:
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    star: false,
+    date: '24 de Fev.',
+  },
+  {
+    id: '7',
+    name: 'Jhon Smith',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/65.jpg',
+    marker: false,
+    subject: 'What is Lorem Ipsum',
+    message:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    star: true,
+    date: '23 de Fev.',
+  },
+  {
+    id: '8',
+    name: 'Anne Clarke',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/66.jpg',
+    marker: false,
+    subject: 'Where does it come from',
+    message:
+      'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+    star: true,
+    date: '24 de Fev.',
+  },
+  {
+    id: '9',
+    name: 'Mike Johnson',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/45.jpg',
+    marker: false,
+    subject: 'Why do we use it?',
+    message:
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    star: false,
+    date: '24 de Fev.',
+  },
+  {
+    id: '10',
+    name: 'Jhon Smith',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/65.jpg',
+    marker: false,
+    subject: 'What is Lorem Ipsum',
+    message:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    star: true,
+    date: '23 de Fev.',
+  },
+  {
+    id: '11',
+    name: 'Anne Clarke',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/66.jpg',
+    marker: false,
+    subject: 'Where does it come from',
+    message:
+      'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+    star: true,
+    date: '24 de Fev.',
+  },
+  {
+    id: '12',
+    name: 'Mike Johnson',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/45.jpg',
+    marker: false,
+    subject: 'Why do we use it?',
+    message:
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    star: false,
+    date: '24 de Fev.',
+  },
+  {
+    id: '13',
+    name: 'Jhon Smith',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/65.jpg',
+    marker: false,
+    subject: 'What is Lorem Ipsum',
+    message:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    star: true,
+    date: '23 de Fev.',
+  },
+  {
+    id: '14',
+    name: 'Anne Clarke',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/66.jpg',
+    marker: false,
+    subject: 'Where does it come from',
+    message:
+      'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+    star: true,
+    date: '24 de Fev.',
+  },
+  {
+    id: '15',
+    name: 'Mike Johnson',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/45.jpg',
+    marker: false,
+    subject: 'Why do we use it?',
+    message:
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    star: false,
+    date: '24 de Fev.',
+  },
+  {
+    id: '16',
+    name: 'Jhon Smith',
+    avatar: 'https://mighty.tools/mockmind-api/content/human/65.jpg',
+    marker: false,
+    subject: 'What is Lorem Ipsum',
+    message:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    star: true,
+    date: '23 de Fev.',
+  },
+]
+
+export type EmailType = (typeof EMAILS)[0]
