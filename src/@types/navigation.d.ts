6ab2c0f8489111d75c18986d77bce20ce77597c4
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-02-26 16:55:54 -0300

    feat: :sparkles: Extends drawer options type

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,9 @@
+import { IconNameProps } from '@/components/DrawerButton'
+import { DrawerNavigationOptions } from '@react-navigation/drawer'
+
+interface CustomOptions extends DrawerNavigationOptions {
+  iconName: IconNameProps
+  isDivider?: boolean
+  notifications?: number
+  sectionTitle?: string
+}
